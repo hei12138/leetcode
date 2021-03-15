bf674504a7a1d6ae34e0b1f570948048eaa037eb
李慎材 <Yealink@2019>
2021-03-15 16:19:23 +0800

    add solution54

--- src/com/hei123/solution/tree/Solution.java
+++ src/com/hei123/solution/tree/Solution.java
@@ -1,4 +1,4 @@
-package com.hei123.solution;
+package com.hei123.solution.tree;
 
 import java.util.LinkedList;
 import java.util.Stack;
@@ -10,7 +10,7 @@ class Solution {
     public static void main(String[] args) {
 //        final String s = transReversePolishNotation("2+(4*3)/2");
 //        System.out.println(calculate2("-2+1"));
-        printTree1(trans2tree("1-2*3"));
+        TreeNode.printTree(trans2tree("1-2*3"));
     }
 
     public static int calculate2(String s) {
@@ -245,48 +245,5 @@ class Solution {
     }
 
 
-    /*
-     * 分行从上到下打印二叉树
-     */
-    // 题目：从上到下按层打印二叉树，同一层的结点按从左到右的顺序打印，每一层
-    // 打印到一行。
-    public static void printTree1(TreeNode root) {
-        if (root == null)
-            return;
-        LinkedList<TreeNode> queue = new LinkedList<TreeNode>();
-        queue.offer(root);
-        TreeNode node = null;
-        int pCount = 0;      //当前层结点数目
-        int nextCount = 1;   //下一层结点数目
-        while (!queue.isEmpty()) {
-            pCount = nextCount;
-            nextCount = 0;
-            //打印当前层数字，并计算下一层结点数目
-            for (int i = 1; i <= pCount; i++) {
-                node = queue.poll();
-                System.out.print(node.val + " ");
-                if (node.left != null) {
-                    queue.offer(node.left);
-                    nextCount++;
-                }
-                if (node.right != null) {
-                    queue.offer(node.right);
-                    nextCount++;
-                }
-            }
-            System.out.println();
-        }
-    }
-}
 
-class TreeNode {
-    public TreeNode left;
-    public TreeNode right;
-    public String val;
-
-    public TreeNode(String val) {
-        this.left = null;
-        this.right = null;
-        this.val = val;
-    }
 }
